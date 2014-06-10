ba5a93761be3ffd9f99fbaa8c034fddf0bb28088
Louis Pennachio <pennachiolouis@gmail.com>
2014-06-10 12:37:56 +0200

    implementation error.c

--- error.c
+++ error.c
@@ -1,7 +1,27 @@
 #include <stdlib.h>
 
+const char *error_names[] = {
+    "No error",
+    "Unknown instruction",
+    "Illegal condition",
+    "Immediate value forbidden",
+    "Segmentation fault in text",
+    "Segmentation fault in data",
+    "Segmentation fault in stack",
+};
+
+const char *warning_names[] = {
+    "HALT",
+};
 
 
-void error(Error err, unsigned addr){}
+void error(Error err, unsigned addr){
+    assert(err <= LAST_ERROR);
+    fprintf(stderr, "ERROR: %s at address 0x%x\n", error_names[err], addr);
+    exit(1);
+}
 
-void warning(Warning warn, unsigned addr){}
+void warning(Warning warn, unsigned addr){
+    assert(warn <= LAST_WARNING);
+    fprintf(stderr, "WARNING: %s reached at 0x%x\n", warning_names[warn], addr);
+}
