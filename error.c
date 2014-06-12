c8fff4159ae16cf67f1a8177fcbdcfd989585525
Garance Vallat <vallat.garance@etu.unice.fr>
2014-06-12 10:41:01 +0200

    ajout doc

--- error.c
+++ error.c
@@ -17,13 +17,23 @@ const char *warning_names[] = {
     "HALT reached",
 };
 
-
+//! Affichage d'une erreur et fin du simulateur
+/*!
+ * 
+ * \param err code de l'erreur
+ * \param addr adresse de l'erreur
+ */
 void error(Error err, unsigned addr){
     assert(err <= LAST_ERROR);
     fprintf(stderr, "ERROR: %s at address 0x%x\n", error_names[err], addr);
     exit(1);
 }
 
+//! Affichage d'un avertissement
+/*!
+ * \param warn code de l'avertissement
+ * \param addr adresse de l'erreur
+ */
 void warning(Warning warn, unsigned addr){
     assert(warn <= LAST_WARNING);
     fprintf(stderr, "WARNING: %s at address 0x%x\n", warning_names[warn], addr);
